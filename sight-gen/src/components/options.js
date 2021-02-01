98970925c0c8338a72d74833d1ab964c26fbd54a
Cameron <36252679+c-roberts@users.noreply.github.com>
2021-02-01 15:50:00 -0600

    init options

---
+++ sight-gen/src/components/options.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {  } from 'antd';
+
+import "antd/dist/antd.css";
+
+
+
+class OptionsMenu extends React.Component {
+    render() {
+        return (
+            <div>
+
+
+            </div>
+            
+        );
+    }
+}
+  
+export default OptionsMenu;
+
